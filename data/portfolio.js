eccd86da147a393332c9ad5f0bbda2b70f47858b
Vincent Liussyaputra <liusvinv@gmail.com>
2023-01-24 01:28:22 +0700

    Add the gallery display functionality

--- data/portfolio.js
+++ data/portfolio.js
@@ -2,14 +2,161 @@
 const portfolio = 
 // Replace Below this ------------------->>
 {
-    "overlays": [
+    "Overlays": [
       {
         "link": "https://delichan.carrd.co/assets/images/gallery06/5710bb05.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery06/36806a17.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery06/32d67bfa.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery06/91dade97.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery06/38231e17.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery06/020672d6.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery06/46c76d3e.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery06/afee0849.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/videos/video01.mp4",
         "type": "video",
-        "size": "small"
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/videos/video03.mp4",
+        "type": "video",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/videos/video04.mp4",
+        "type": "video",
+        "size": "large"
+      }
+    ],
+    "Panels": [
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery07/e802c2f2.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery07/3fb9110d.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery07/c76c239f.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery07/57a3d467.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery07/54f9628c.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery07/7ff12756.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery07/d6195430.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery07/d598f073.jpg",
+        "type": "image",
+        "size": "medium"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/image10.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/27c352d5.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/e0d1d28e.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/8c60135a.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/d282832d.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/8c60135a.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/d7ecee2a.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/3fca84db.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/32130216.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/d9ba05e7.jpg",
+        "type": "image",
+        "size": "large"
+      },
+      {
+        "link": "https://delichan.carrd.co/assets/images/gallery08/0253263c.jpg",
+        "type": "image",
+        "size": "large"
       }
     ]
-}
+  }
 // Replace above this ------------------->>
 ;
 
--- scripts/portfolio_handler.js
+++ scripts/portfolio_handler.js
@@ -1,7 +1,39 @@
 import portfolio from '../data/portfolio.js';
 
-function showPortfolio(gallery){
-    console.log(gallery,portfolio);
+// Display All Gallery item
+function showPortfolio(gallery,parentEl){
+    let imageList = portfolio[gallery] || null;
+    if(!imageList){
+        console.log("No Category is found!! Check the source code and make sure the category exists!");
+        return;
+    }
+
+    console.log(gallery);
+    imageList.forEach(list => {
+        // Add item to the specified element
+        parentEl.appendChild(createGalleryItem(list));
+    });
+    
+}
+// Create a gallery item element
+function createGalleryItem({link,type,size}){
+    // Create Element Type
+    let galleryEl = document.createElement(type === "video" ? 'video' : 'img');
+    // Set the source
+    galleryEl.src = link;
+
+    if(type === 'video'){
+        galleryEl.setAttribute('controls','');
+        galleryEl.setAttribute('loop','');
+    }
+    // Add lazy loading
+    galleryEl.loading = "lazy";
+    galleryEl.classList.add(size);
+
+    galleryEl.addEventListener('click',()=>{
+        window.open(link,'_blank');
+    });
+    return galleryEl;
 }
 
 
