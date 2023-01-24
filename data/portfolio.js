d57641c63fc844e2a7961a7018c07bdfe6d8c9f6
Vincent Liussyaputra <liusvinv@gmail.com>
2023-01-24 21:48:31 +0700

    Add the schedule page

--- data/portfolio.js
+++ data/portfolio.js
@@ -2,161 +2,283 @@
 const portfolio = 
 // Replace Below this ------------------->>
 {
-    "Overlays": [
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery06/5710bb05.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery06/36806a17.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery06/32d67bfa.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery06/91dade97.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery06/38231e17.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery06/020672d6.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery06/46c76d3e.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery06/afee0849.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/videos/video01.mp4",
-        "type": "video",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/videos/video03.mp4",
-        "type": "video",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/videos/video04.mp4",
-        "type": "video",
-        "size": "large"
-      }
-    ],
-    "Panels": [
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery07/e802c2f2.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery07/3fb9110d.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery07/c76c239f.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery07/57a3d467.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery07/54f9628c.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery07/7ff12756.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery07/d6195430.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery07/d598f073.jpg",
-        "type": "image",
-        "size": "medium"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/image10.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/27c352d5.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/e0d1d28e.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/8c60135a.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/d282832d.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/8c60135a.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/d7ecee2a.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/3fca84db.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/32130216.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/d9ba05e7.jpg",
-        "type": "image",
-        "size": "large"
-      },
-      {
-        "link": "https://delichan.carrd.co/assets/images/gallery08/0253263c.jpg",
-        "type": "image",
-        "size": "large"
-      }
-    ]
-  }
+  "Overlays": [
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery06/5710bb05.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery06/36806a17.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery06/32d67bfa.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery06/91dade97.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery06/38231e17.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery06/020672d6.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery06/46c76d3e.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery06/afee0849.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/videos/video01.mp4",
+      "type": "video",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/videos/video03.mp4",
+      "type": "video",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/videos/video04.mp4",
+      "type": "video",
+      "size": "large"
+    }
+  ],
+  "Schedules": [
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/e3f9944d.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/fbe05a3e.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/f2d72a68.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/719abf32.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/8d8c099c.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/1b9a5e18.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/3ec789da.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/58cfcb30.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/a90b4659.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/e7ac32c6.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/022a3343.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/88741fec.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/d0dd05b4.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/270ef006.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/dd608a9c.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/fbc8f23f.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/d2b0d092.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/31216ccb.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/121e68f1.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/84eb39db.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/01575a8b.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/05348ccb.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/dde9e442.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery03/1505a85f.jpg",
+      "type": "image",
+      "size": "medium"
+    }
+  ],
+  "Panels": [
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery07/e802c2f2.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery07/3fb9110d.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery07/c76c239f.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery07/57a3d467.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery07/54f9628c.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery07/7ff12756.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery07/d6195430.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery07/d598f073.jpg",
+      "type": "image",
+      "size": "medium"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/image10.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/27c352d5.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/e0d1d28e.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/8c60135a.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/d282832d.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/8c60135a.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/d7ecee2a.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/3fca84db.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/32130216.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/d9ba05e7.jpg",
+      "type": "image",
+      "size": "large"
+    },
+    {
+      "link": "https://delichan.carrd.co/assets/images/gallery08/0253263c.jpg",
+      "type": "image",
+      "size": "large"
+    }
+  ]
+}
 // Replace above this ------------------->>
 ;
 
